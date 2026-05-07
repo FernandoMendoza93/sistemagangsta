216b1f408155bb09502d594d4270056caba008dc
Fernando Mendoza <angfer101@hotmail.com>
2026-05-06 22:17:33 -0600

    feat: premium landing page portal redesign

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -348,7 +348,9 @@ router.get('/barberia-info/:slug', async (req, res) => {
 
         const barberia = await dbQuery.get(
             `SELECT b.id, b.nombre, b.logo_url, b.color_acento, b.activo, b.telefono_whatsapp,
-                    b.landing_titulo, b.landing_descripcion, b.landing_imagen_fondo
+                    b.landing_titulo, b.landing_descripcion, b.landing_imagen_fondo,
+                    b.bg_main, b.bg_surface, b.accent_primary, b.accent_secondary,
+                    b.text_main, b.text_muted, b.clase_glass
              FROM barberias b
              WHERE b.slug = ?`,
             [slug]
